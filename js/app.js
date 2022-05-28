4c4e5808fb194007c03bbf87a043d030b3863fa3
Rishi-077 <rishikesan070@gmail.com>
2022-05-28 22:53:17 +0530

    fully completed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,63 @@
-$('.grid').isotope({
+const skills_wrap = document.querySelector( '.skills' );
+const skills_bars = document.querySelectorAll( '.skill-progress' );
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
+
+$( '.grid' ).isotope( {
   // options
   itemSelector: '.grid-item',
   layoutMode: 'fitRows',
   transitionDuration:'0.6s',
 });
+
+window.addEventListener( 'scroll', () =>
+{
+  skillsEffect();
+})
+
+function checkScroll ( el )
+{
+  let rect = el.getBoundingClientRect();
+  
+  if ( window.innerHeight >= rect.top + el.offsetHeight ) return true;
+  return false;
+}
+
+function skillsEffect ()
+{
+  if ( !checkScroll( skills_wrap ) ) return;
+  skills_bars.forEach( skill => skill.style.width = skill.dataset.progress );
+}
+
+
+var mySwiper = new Swiper(".swiper-container", {
+  speed: 1100,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+});
+
+
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
